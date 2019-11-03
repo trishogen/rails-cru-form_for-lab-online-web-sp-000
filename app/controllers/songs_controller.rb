d43d7c2cbdc285459b4c352e8d72c175441ed1a2
Trish Ogen <patriciaogen@gmail.com>
2019-11-03 17:04:54 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -6,4 +6,19 @@ class SongsController < ApplicationController
     @artist = @song.artist
   end
 
+  def new
+    @song = Song.new
+  end
+
+  def create
+    @song.Song.new(song_params)
+    @song.save
+    redirect_to song_path(@song)
+  end
+
+  private
+
+  def song_params
+    params.require(:song).permit(:name)
+  end
 end
