b908285e808b0e8768a99fcd10eb2abe07d9ca39
Trish Ogen <patriciaogen@gmail.com>
2019-11-03 17:07:31 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -11,7 +11,7 @@ class SongsController < ApplicationController
   end
 
   def create
-    @song.Song.new(song_params)
+    @song = Song.new(song_params)
     @song.save
     redirect_to song_path(@song)
   end
